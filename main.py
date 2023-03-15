8062a7ff0e57ef0fcb4812a03076e195b9e002e4
emi~ <esauvisky@gmail.com>
2023-03-14 23:38:41 -0300

    refactor: notebook

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ from loguru import logger
 from midi2audio import FluidSynth
 from tqdm import tqdm
 
-SEQUENCE_LENGTH = 16
-BATCH_SIZE = 16
-EPOCHS = 300
+SEQUENCE_LENGTH = 32
+BATCH_SIZE = 8
+EPOCHS = 100
 QL_TO_ORDINAL = {}
 ORDINAL_TO_QL = {}
-
-
 
 def setup_logging(level="DEBUG", show_module=False):
     """
@@ -66,16 +64,12 @@ def play_song(song):
     stream.write('midi', fp='generated_music.mid')
     sf2_path = '/usr/share/soundfonts/freepats-general-midi.sf2' # path to sound font file
     FluidSynth(sound_font=sf2_path).play_midi("generated_music.mid")
-
-
 def parse_corpus(corpi):
     notes = []
     for corpus in corpi:
         logger.info(f'Parsing {corpus}')
-        # sf2_path = '/usr/share/soundfonts/freepats-general-midi.sf2' # path to sound font file
-        # FluidSynth(sound_font=sf2_path).play_midi(corpus)
         parsed = music21.converter.parse(corpus)
-        elements = parsed.flat.notesAndRests
+        elements = parsed.elements[1].flat.notesAndRests
         for element in elements:
             # duration = element.duration.fullName # too many different values
             duration = element.duration.quarterLengthNoTuplets # less precise
@@ -104,41 +98,37 @@ def parse_corpus(corpi):
     max_len = max(len(note) for note in notes)
     notes = [note + [0] * (max_len - len(note)) for note in notes]
     return notes
-
-
 ## Load the midi files
 # corpi = [c for c in music21.corpus.getComposer("mozart")[2:5]]
 # corpi = [os.path.join('./midi', f) for f in os.listdir('./midi') if f.endswith('.mid')]
-# corpi = [os.path.join('./midi', f) for f in os.listdir('./midi') if f.endswith('.mid') and f.startswith("mid")][:20]
+# corpi = [os.path.join('./midi', f) for f in os.listdir('./midi') if f.endswith('.mid') and f.startswith("mid")][5:10]
 # corpi = [os.path.join('./midi/pokemon', f) for f in os.listdir('./midi/pokemon')]
 corpi = ["./midi/pokemon/Pokemon_game_-_Pokemon.mid"]
-notes = parse_corpus(corpi)
+NOTES = parse_corpus(corpi)
 
 ## Preprocess the data
-sequence_length = SEQUENCE_LENGTH
-n_samples = (len(notes) // BATCH_SIZE) * BATCH_SIZE
-notes = notes[:n_samples]
-num_features = len(notes[0])
+NUM_NOTES = (len(NOTES) // BATCH_SIZE) * BATCH_SIZE
+NOTES = NOTES[:NUM_NOTES]
+NUM_FEATURES = len(NOTES[0])
 
-feature_classes_count = [max(np.max(notes, axis=0)[i] + 1 for notes in [notes]) for i in range(num_features)]
-features = [np.array([note[i] for note in notes]) for i in range(num_features)]
+feature_classes_count = [max(np.max(notes, axis=0)[i] + 1 for notes in [NOTES]) for i in range(NUM_FEATURES)]
+features = [np.array([note[i] for note in NOTES]) for i in range(NUM_FEATURES)]
 Xs = [
-    np.zeros((len(notes) - sequence_length, sequence_length, feature_classes_count[i]), dtype=np.int32)
-    for i in range(num_features)]
-ys = [np.zeros((len(notes) - sequence_length, feature_classes_count[i]), dtype=np.int32) for i in range(num_features)]
+    np.zeros((NUM_NOTES - SEQUENCE_LENGTH, SEQUENCE_LENGTH, feature_classes_count[i]), dtype=np.int32)
+    for i in range(NUM_FEATURES)]
+ys = [np.zeros((NUM_NOTES - SEQUENCE_LENGTH, feature_classes_count[i]), dtype=np.int32) for i in range(NUM_FEATURES)]
 
-for i in range(num_features):
-    for j in range(len(notes) - sequence_length):
-        Xs[i][j] = to_categorical(features[i][j:j + sequence_length], num_classes=feature_classes_count[i])
-        ys[i][j] = to_categorical(features[i][j + sequence_length], num_classes=feature_classes_count[i])
-
+for i in range(NUM_FEATURES):
+    for j in range(NUM_NOTES - SEQUENCE_LENGTH):
+        Xs[i][j] = to_categorical(features[i][j:j + SEQUENCE_LENGTH], num_classes=feature_classes_count[i])
+        ys[i][j] = to_categorical(features[i][j + SEQUENCE_LENGTH], num_classes=feature_classes_count[i])
 # Define the model architecture
-inputs = [Input(shape=(sequence_length, feature_classes_count[i])) for i in range(num_features)]
+inputs = [Input(shape=(SEQUENCE_LENGTH, feature_classes_count[i])) for i in range(NUM_FEATURES)]
 merged = concatenate(inputs)
 
-lstm1 = LSTM(units=64, activation='tanh', return_sequences=True)(merged)
-lstm2 = LSTM(units=64)(lstm1)
-outputs = [Dense(feature_classes_count[i], activation='softmax', name=f'out{i}')(lstm2) for i in range(num_features)]
+lstm1 = LSTM(units=128, activation='tanh', return_sequences=True)(merged)
+lstm2 = LSTM(units=128)(lstm1)
+outputs = [Dense(feature_classes_count[i], activation='softmax', name=f'out{i}')(lstm2) for i in range(NUM_FEATURES)]
 
 model = Model(inputs=inputs, outputs=outputs)
 
@@ -147,35 +137,38 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categ
 
 # Train the model
 history = model.fit(Xs, ys, batch_size=BATCH_SIZE, epochs=EPOCHS, use_multiprocessing=True)
-
-
-def predict_sequence(model, seed_sequence, sequence_length, num_features):
+def predict_sequence(model, seed_sequence, num_features):
     # Encode the seed sequence
-    seed_sequence = [seed_sequence[i][-sequence_length:] for i in range(num_features)]
-    seed_sequence = [to_categorical([seed_sequence[i]], num_classes=feature_classes_count[i]) for i in range(num_features)]
+    slen = len(seed_sequence[0])
+    seed_sequence = [seed_sequence[i][-slen:] for i in range(num_features)]
+    seed_sequence = [
+        to_categorical([seed_sequence[i]], num_classes=feature_classes_count[i]) for i in range(num_features)]
     # Predict the next feature values in the sequence
     predicted_features = model.predict(seed_sequence, verbose=0)
     # Decode the predicted feature values
     next_features = [np.argmax(predicted_features[i][0]) for i in range(num_features)]
-
     return next_features
 
 
-def generate_song(model, seed_sequence, sequence_length, song_length, num_features):
-    # song = [list(reversed(seed_sequence[i][:sequence_length])) for i in range(num_features)]
-    song = [seed_sequence[i][:sequence_length] for i in range(num_features)]
-    for i in range(song_length - sequence_length):
-        next_features = predict_sequence(model, song, sequence_length, num_features)
+def generate_song(model, seed_sequence, generated_length, num_features):
+    slen = len(seed_sequence[0])
+    current_sequence = [list(reversed(seed_sequence[i][:slen])) for i in range(num_features)]
+    # song = [seed_sequence[i][:slen] for i in range(num_features)]
+    generated = np.asarray([[] for i in range(num_features)], dtype=int)
+    for i in range(generated_length - slen):
+        next_features = predict_sequence(model, current_sequence, num_features)
         print(f"Predicted feature: {next_features}")
-        song = [np.append(song[i], next_features[i]) for i in range(num_features)]
-    return song
+        current_sequence = np.hstack((current_sequence, np.transpose([next_features])))
+        current_sequence = np.delete(current_sequence, 0, axis=1)
+        generated = np.hstack((generated, np.transpose([next_features])), dtype=int)
+    return generated
 
 
 # Set the seed sequence and the desired length of the generated song
-seed_sequence = [features[i][:SEQUENCE_LENGTH] for i in range(num_features)]
-song_length = 128
+seed_sequence = [features[i][:SEQUENCE_LENGTH] for i in range(NUM_FEATURES)]
+GENERATED_LENGTH = 50
 
 # Generate the song
-song = generate_song(model, seed_sequence, sequence_length, song_length, num_features)
+song = generate_song(model, seed_sequence, GENERATED_LENGTH, NUM_FEATURES)
 song = np.transpose(song)
 play_song(song)
